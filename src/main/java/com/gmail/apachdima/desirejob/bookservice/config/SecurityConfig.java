a60893e38211f531054a8f9c9c4e1c8decc9dc15
Kovalenko Dmytro <kovalenko.dm80@gmail.com>
2023-08-10 16:19:53 +0300

    [+/*/-] Keycloak Auth server security added

--- src/main/java/com/gmail/apachdima/desirejob/bookservice/config/SecurityConfig.java
+++ src/main/java/com/gmail/apachdima/desirejob/bookservice/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.gmail.apachdima.desirejob.bookservice.config;
 
+import com.gmail.apachdima.desirejob.bookservice.util.converter.JwtAuthConverter;
+import com.gmail.apachdima.desirejob.commonservice.constant.OpenApiAsset;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
@@ -19,41 +16,28 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final PasswordEncoder passwordEncoder;
+    private final JwtAuthConverter jwtAuthConverter;
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-        return configuration.getAuthenticationManager();
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            .cors()
             .and()
+            .csrf().disable()
             .authorizeRequests()
-            .anyRequest().authenticated()
+            .antMatchers(OpenApiAsset.getAssets()).permitAll()
+            .anyRequest()
+            .authenticated()
             .and()
             .formLogin().disable()
             .httpBasic().disable()
-            .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+            .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .and()
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(jwtAuthConverter);
 
         return http.build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) ->
-            web.ignoring()
-                .antMatchers("/api-docs",
-                    "/api-docs/**",
-                    "/v3/api-docs/**",
-                    "/configuration/ui",
-                    "/swagger-resources/**",
-                    "/configuration/security",
-                    "/webjars/**",
-                    "/swagger-ui/**");
-    }
 }
